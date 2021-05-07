4c70f453fbc169a43874ccdfbc0df719da0e5fbf
Tarun Thota <tarunthota3@gmail.com>
2021-05-07 01:06:10 -0400

    modified the stucture of code

---
+++ routes/detectPPERouter.js
@@ -0,0 +1,91 @@
+const router = require("express").Router();
+var multer = require('multer')
+var AWS = require('aws-sdk')
+var fs = require('fs')
+var path = require('path')
+require("dotenv").config();
+
+var localFileName = "";
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + '/../images/')
+    },
+    filename: function (req, file, cb) {
+        localFileName = Date.now() + "-" + file.originalname
+        cb(null, localFileName)
+    }
+});
+
+var upload = multer({ 
+    storage: storage,
+    fileFilter: function (req, file, callback) {
+        var ext = path.extname(file.originalname);
+        if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+            return callback(new Error('Only images are allowed'))
+        }
+        callback(null, true)
+    }
+})
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+})
+
+function getFile(path) {
+    try {
+        const image = fs.readFileSync(path)
+        return (image)
+    }
+    catch (e) {
+        console.error(e.message)
+        process.exit()
+    }
+}
+const rekognition = new AWS.Rekognition();
+
+
+/**
+ * @swagger
+ * /detectPPE:
+ *   post:
+ *     description: Return the PPE detection of an image uploaded
+ *     consumes:
+ *       - multipart/form-data
+ *     parameters:
+ *       - in: formData
+ *         name: image
+ *         type: file
+ *         required: true
+ *         description: The file to upload.
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Response from AWS rekognition with details of PPE detection
+ *       400:
+ *         description: File can't be empty
+ *       500:
+ *         description: Only images are allowed
+ */ 
+router.post('/', upload.any(), function (req, res) {
+    if(localFileName.length == 0)
+        res.status(400).send("File can't be empty");
+    
+    var params = {
+        Image: {
+            Bytes: getFile(`./images/${localFileName}`)
+        }
+    };
+    
+    rekognition.detectProtectiveEquipment(params, function (err, data) {
+        if (err) console.log("err: ", err, err.stack);
+        else {
+            console.log("data: ", data);
+            res.json(data);
+        }
+    });
+});
+
+module.exports = router;
